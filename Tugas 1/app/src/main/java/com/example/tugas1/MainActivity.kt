d6fbee79801e01167f7e92ff622b8378c07e22ef
Noval Raihan R <Sleepyneko23@gmail.com>
2024-08-23 10:37:27 +0700

    Upload Tugas1
    Tugas 1 Pengembangan Aplikasi Mobile Kelas B

---
+++ Tugas 1/app/src/main/java/com/example/tugas1/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.tugas1
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var nama : TextView
+    lateinit var nim : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        nama = findViewById(R.id.nama)
+        nim = findViewById(R.id.nim)
+
+        nama.text = "Noval Raihan Ramadhan"
+        nim.text = "225150407111056"
+    }
+}
